cce80e4a1d100c8bbf867f069536fb66a33d4d1b
Konstantin Shpolvind <Konstantin.Shpolvind@acronis.com>
2017-01-17 23:14:39 +0300

    Header + Footer for mobile device

--- index.js
+++ index.js
@@ -11,12 +11,27 @@ window.addEventListener('load',function () {
         el: '#header',
         data: {
             visible: true,
-            companyLogo: {
+            logo: {
+                visible: true,
                 height: 100,
                 width:100,
                 src: companyLogos[3].src,
                 alt: "название компании, самая клевая компания"
+            },
+            brief: {
+                visible: true
+            },
+            phones: {
+                visible: true
             }
+
+        }
+    });
+
+    scope.footer = new Vue({
+        el: 'footer',
+        data: {
+            visible: true
         }
     });
 
